bf18c6919a10146322146860125fe9c02e630d18
Nico Breßler <nico.bressler@db-n.com>
2019-06-14 15:51:49 +0200

    formatting

--- webpack.common.js
+++ webpack.common.js
@@ -1,77 +1,69 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: [
-        './src/js/index.js'
-    ],
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'src/index.html'
-        }),
-        new CopyWebpackPlugin([
-            {from: 'src/img/', to: '', flatten: false}
-        ]),
-    ],
-    output: {
-        filename: 'app.js',
-        path: path.resolve(__dirname, 'www'),
-        publicPath: '/'
-    },
-    resolve: {
-        extensions: ['.*', '.js', '.json', '.scss']
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: [
-                'babel-loader',
-                'import-glob'
-            ]
-            },
-            {
-                test: /\.(scss|css)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'sass-loader'
-                    },
-                    'import-glob',
-                ]
-            },
-            {
-                test: /\.svg$/,
-                loader: 'raw-loader'
-            },
-            {
-                test: /\.(jpe?g|png|gif)$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: "[name].[ext]"
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.(eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[ext]"
-                        },
-                    },
-                ],
-            },
-
+  entry: ['./src/js/index.js'],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CopyWebpackPlugin([{ from: 'src/img/', to: '', flatten: false }])
+  ],
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'www'),
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['.*', '.js', '.json', '.scss']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'import-glob']
+      },
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader'
+          },
+          'import-glob'
         ]
-    }
-};
+      },
+      {
+        test: /\.svg$/,
+        loader: 'raw-loader'
+      },
+      {
+        test: /\.(jpe?g|png|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(eot|otf|webp|ttf|woff|woff2)(\?.*)?$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
